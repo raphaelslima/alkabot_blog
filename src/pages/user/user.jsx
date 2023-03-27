6977153488f0b8dbf2299329c941056c7ba30c7b
Raphael Lima <raphaeldesousalm@gmail.com>
2023-03-27 11:15:13 -0300

    btn close na pag de user

--- src/pages/user/user.jsx
+++ src/pages/user/user.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 import "./user.css";
 
@@ -26,6 +26,23 @@ export default () => {
     <div className="container-perfil">
       <div className="perfil">
         <h2 className="title-section">{user.name}</h2>
+        <Link to={"/"}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </Link>
+
         <div className="line"></div>
         <section className="information">
           <label htmlFor="">Telefone:</label>
